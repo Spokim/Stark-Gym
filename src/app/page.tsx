9897dd26ceba967aa2459a190d8ec46287a6450c
[Markus Svanberg] <[svanberg.markus@yahoo.se]>
2023-12-08 00:36:32 +0100

    Open graph implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,20 @@ export const metadata: Metadata = {
   title: "Stark Gym | Get Strong, Get Fit, Get Results",
   description:
     "Join Stark Gym and unlock your full potential. We are dedicated to helping you achieve your fitness goals with state-of-the-art facilities, expert trainers, and a motivating environment.",
+  openGraph: {
+    title: "Stark Gym | Get Strong, Get Fit, Get Results",
+    description:
+      "Join Stark Gym and unlock your full potential. We are dedicated to helping you achieve your fitness goals with state-of-the-art facilities, expert trainers, and a motivating environment.",
+    images: [
+      {
+        url: "stark-gym.vercel.app/public/images/CTA-01.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Stark Gym",
+      },
+    ],
+    siteName: "Stark Gym",
+  },
 };
 
 export default function Home() {
